2b81e1dc9c48aa393502942692753de320fd69dd
Giovanni <gaardilatroncoso@hotmail.com>
2023-05-31 19:05:54 -0500

    problemas de enviar mismo mensaje

--- src/controllers/whatsapp.controller.ts
+++ src/controllers/whatsapp.controller.ts
@@ -5,78 +5,78 @@ import bot from '../utils/bot.util'
 export default class WhatsappController {
   receivedMessageWhatsapp ({ body }: Request, res: Response): void {
     try {
-      body = body.entry[0].changes[0].value
-      taskService.getTaskById(body.contacts[0].wa_id)
-        .then((responseGetTaskById) => {
-          let indexTasks = 0
-          let maxSeconds = 0
-          let index = 0
-          responseGetTaskById.forEach((task) => {
-            console.log(task.data())
-            if (task.data().timestamp.seconds > maxSeconds) {
-              maxSeconds = task.data().timestamp.seconds
-              indexTasks = index
-            }
-            index++
-          })
-          if (responseGetTaskById[indexTasks] === undefined || responseGetTaskById[indexTasks].data().status === 'CLOSE') {
-            bot.processNewTask(body, res).then(() => {
-              console.log('nuevo')
-            }).catch(() => {})
-          } else {
-            let isMenu = false
-            let type = ''
-            Object.entries(body.messages[0]).forEach(([key, _value]) => {
-              if (key === 'button') {
-                if (body.messages[0].button.text === 'Volver al menú' || body.messages[0].button.text === 'Ingresar identificación') {
-                  isMenu = true
-                  type = body.messages[0].button.text
-                }
+      if (body?.entry[0]?.changes[0]?.value?.messages !== undefined) {
+        body = body.entry[0].changes[0].value
+        taskService.getTaskById(body.contacts[0].wa_id)
+          .then((responseGetTaskById) => {
+            let indexTasks = 0
+            let maxSeconds = 0
+            let index = 0
+            responseGetTaskById.forEach((task) => {
+              if (task.data().timestamp.seconds > maxSeconds) {
+                maxSeconds = task.data().timestamp.seconds
+                indexTasks = index
               }
+              index++
             })
-            if (isMenu) {
-              switch (type) {
-                case 'Volver al menú':{
-                  void taskService.updateTask(responseGetTaskById[indexTasks].id, {
-                    status: 'menu'
-                  })
-                  void whatsappService.sendMessageWhatsapp(
-                    {
-                      bodyText: 'Por favor, selecciona el tipo de asistencia que necesitas de las opciones a continuación:',
-                      buttons: {
-                        Consulta: 'Consultas',
-                        Documento: 'Documentación'
-                      },
-                      options: {
-                        // Opciones adicionales, si es necesario
-                      }
-                    },
-                    'button',
-                    String(process.env.ID_NUMBER),
-                    String(process.env.WP_TOKEN),
-                    body.messages[0].from)
-                  break
-                }
-                case 'Ingresar identificación':{
-                  const array = responseGetTaskById[indexTasks].data().sequence_task
-                  const arreglo = []
-                  arreglo.push(array[0])
-                  void taskService.updateTask(responseGetTaskById[indexTasks].id, {
-                    sequence_task: arreglo,
-                    status: 'pin'
-                  })
-                  break
-                }
-              }
+            if (responseGetTaskById[indexTasks] === undefined || responseGetTaskById[indexTasks].data().status === 'CLOSE') {
+              bot.processNewTask(body, res).then(() => {
+                console.log('nuevo')
+              }).catch(() => {})
             } else {
-              void bot.processExistingTask(body, responseGetTaskById[indexTasks], res)
+              let isMenu = false
+              let type = ''
+              const title = body.messages[0]?.interactive?.button_reply?.title
+              if (title === 'Volver al menú' || title === 'Ingresar identificación') {
+                isMenu = true
+                type = title
+              }
+              if (isMenu) {
+                switch (type) {
+                  case 'Volver al menú':{
+                    void taskService.updateTask(responseGetTaskById[indexTasks].id, {
+                      status: 'menu'
+                    })
+                    void whatsappService.sendMessageWhatsapp(
+                      {
+                        bodyText: 'Por favor, selecciona el tipo de asistencia que necesitas de las opciones a continuación:',
+                        buttons: {
+                          Consulta: 'Consultas',
+                          Documento: 'Documentación'
+                        },
+                        options: {
+                        // Opciones adicionales, si es necesario
+                        }
+                      },
+                      'button',
+                      String(process.env.ID_NUMBER),
+                      String(process.env.WP_TOKEN),
+                      body.messages[0].from)
+                    break
+                  }
+                  case 'Ingresar identificación':{
+                    const array = responseGetTaskById[indexTasks].data().sequence_task
+                    const arreglo = []
+                    arreglo.push(array[0])
+                    void taskService.updateTask(responseGetTaskById[indexTasks].id, {
+                      sequence_task: arreglo,
+                      status: 'pin'
+                    })
+                    break
+                  }
+                }
+              } else {
+                void bot.processExistingTask(body, responseGetTaskById[indexTasks], res)
+              }
             }
-          }
-          res.status(200).send('RECEIVED')
-        }).catch((error) => {
-          console.error(error)
-          res.status(400).send('NOT_RECEIVED')
-        })
+            res.status(200).send('RECEIVED')
+          }).catch((error) => {
+            console.error(error)
+            res.status(400).send('NOT_RECEIVED')
+          })
+      } else {
+        res.status(200).send('RECEIVED')
+      }
     } catch (error) {
       res.status(400).send('NOT_RECEIVED')
     }
